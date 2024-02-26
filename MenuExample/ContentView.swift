ab0128ddfccb85391d0ae30866c8b5e2f0dc438b
Mark Phillips <phillipsnb@cardiff.ac.uk>
2024-02-26 21:05:29 +0000

    Initial Commit

---
+++ MenuExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenuExample
+//
+//  Created by Mark Phillips on 26/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenuExample/MenuExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuExampleApp.swift
+//  MenuExample
+//
+//  Created by Mark Phillips on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
